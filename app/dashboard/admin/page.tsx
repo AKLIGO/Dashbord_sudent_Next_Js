ce0388403d9d4baf9a97a22413d82982245f52bb
AKLIGO <koffitsejeromea@gmail.com>
2026-05-20 17:23:29 +0000

    mise en place du component countChart

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,18 +1,38 @@
+"use client"
+
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const admin = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/*left side  */}
-            <div className="w-full lg:w-2/3 ">
+            <div className="w-full lg:w-2/3 flex flex-col">
                 {/*User Card  */}
-                <div className="flex gap-4 justify-between">
+                <div className="flex gap-4 justify-between flex-wrap">
                     <UserCard type="student" />
                     <UserCard type="teacher" />
                     <UserCard type="parent" />
                     <UserCard type="Staff" />
 
                 </div>
+                {/*MIDDLE CHARTS*/}
+
+                <div className="flex gap-4 flex-col lg:flex-row my-4">
+                    {/* STUDENT STATS */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+
+                    {/* ATTENDANCE CHART */}
+                    <div className="w-full lg:w-2/3 h-[450px]"></div>
+
+                </div>
+
+                {/* BOTTOM CHARTS */}
+                <div className=""></div>
+
+
             </div>
             {/*right side  */}
             <div className="w-full lg:w-1/3 ">r</div>
---
+++ components/CountChart.tsx
@@ -0,0 +1,82 @@
+import Image from 'next/image';
+import { RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+// import { RechartsDevtools } from '@recharts/devtools';
+
+// #region Sample data
+const data = [
+    {
+        name: 'Total',
+        count: 110,
+        fill: 'white',
+    },
+    {
+        name: 'Boys',
+        count: 50,
+        fill: '#FAE27C',
+    },
+    {
+        name: 'Girl',
+        count: 60,
+        fill: '#C3EBFA',
+    },
+];
+
+// #endregion
+const style = {
+    top: '50%',
+    right: 0,
+    transform: 'translate(0, -50%)',
+    lineHeight: '24px',
+};
+
+
+const CountChart = () => {
+    return (
+        <div className='bg-white rounded-xl w-full h-full p-4'>
+            {/*Title*/}
+            <div className='flex justify-between items-center'>
+                <h1 className="text-xl font-semibold">Students</h1>
+                <Image src="/moreDark.png" alt="more" width={20} height={20} />
+            </div>
+            {/*CHART*/}
+            <div className='relative w-full h-[75%]'>
+                <ResponsiveContainer width="100%" height="100%">
+                    <RadialBarChart
+                        cx="50%" cy="50%" innerRadius="40%" outerRadius="100%"
+                        barSize={32} data={data}>
+                        <RadialBar
+
+                            // label={{
+                            //     position: "insideStart",
+                            //     fill: "#fff"
+                            // }}
+                            background
+
+                            dataKey="count"
+
+                        />
+                        {/* <Legend layout="vertical" verticalAlign="middle" /> */}
+                    </RadialBarChart>
+                </ResponsiveContainer>
+                <Image src="/malefemale.png" alt="chart" width={50} height={50} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' />
+            </div>
+            {/*NOTTOM */}
+            <div className='flex justify-center gap-16'>
+                <div className='flex flex-col gap-1'>
+                    <div className='w-5 h-5 bg-lamaSky rounded-full' />
+                    <h1 className='font-bold'>1,234</h1>
+                    <h2 className='text-xs text-gray-300'>Boys (55%)</h2>
+                </div>
+
+                <div className='flex flex-col gap-1'>
+                    <div className='w-5 h-5 bg-lamaYellow rounded-full' />
+                    <h1 className='font-bold'>1,234</h1>
+                    <h2 className='text-xs text-gray-300'>Girl (45%)</h2>
+                </div>
+            </div>
+            {/**/}
+            <div className=''></div>
+        </div>
+    )
+}
+export default CountChart;
