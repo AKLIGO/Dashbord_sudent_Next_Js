f43dc186b90d32935875968571a5c6965154ce1b
AKLIGO <koffitsejeromea@gmail.com>
2026-05-20 15:40:20 +0000

    debut de mise en place du tableau de bord coter admin

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,7 +1,21 @@
+import UserCard from "@/components/UserCard";
+
 const admin = () => {
     return (
-        <div>
-            <h1>adminPage</h1>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+            {/*left side  */}
+            <div className="w-full lg:w-2/3 ">
+                {/*User Card  */}
+                <div className="flex gap-4 justify-between">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="Staff" />
+
+                </div>
+            </div>
+            {/*right side  */}
+            <div className="w-full lg:w-1/3 ">r</div>
         </div>
     )
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
+
 export default function DashboardLayout({
     children,
 }: {
@@ -10,17 +12,19 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
 
-            <div className="w-[14%] md:w[16%] lg:w-[16%] xl:w-[14%] ">
+            {/* LEFT */}
+            <div className="w-[14%] md:w-[16%] lg:w-[16%] xl:w-[14%] p-4">
                 <Link href="/" className="flex items-center lg:justify-start gap-2">
                     <Image src="/logo.png" alt="logo" width={33} height={33} />
-                    <span className="hidden lg:block">School Management System</span>
+                    <span className="hidden lg:block font-bold">School Management</span>
                 </Link>
                 <Menu />
             </div>
 
-            <div className="w-[86%] md:[92%] lg-w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+            {/* RIGHT */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
                 <Navbar />
-                <div>
+                <div className="p-4 flex-1">
                     {children}
                 </div>
             </div>
